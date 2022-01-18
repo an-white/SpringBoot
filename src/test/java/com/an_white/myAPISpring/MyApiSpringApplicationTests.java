46474b7ac721a9cdc59845a5331ffce955b09386
An_White <andresblancahernandez@gmail.com>
2022-01-18 19:24:43 -0400

    start Spring project

---
+++ src/test/java/com/an_white/myAPISpring/MyApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.an_white.myAPISpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
