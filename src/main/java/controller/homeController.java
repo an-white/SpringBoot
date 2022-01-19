ee82c48084fe7fa55d2931980c49b020600a563c
An_White <andresblancahernandez@gmail.com>
2022-01-19 16:17:10 -0400

    added controllers basic and rendering html in an endpoint

--- src/main/java/controller/homeController.java
+++ src/main/java/controller/homeController.java
@@ -3,8 +3,6 @@ package controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 
 /* debe colocarse a las clases de los controladores y deben estar en la misma carpeta que el spring app para poder
